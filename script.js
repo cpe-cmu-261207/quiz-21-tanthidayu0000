49ea897291d02af478e432d3c63126d021e2de1a
tanthidayu0000 <90751261+tanthidayu0000@users.noreply.github.com>
2022-08-04 14:11:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const btnRandom = document.getElementById("btn-random");
+const imgProfile = document.getElementById("img-profile");
+const Gender = document.getElementById("span-gender-icon");
+const UserCard = document.getElementById("div-user-card");
+
 async function callApi() {
+  btnRandom.innerText = "Loading.....";
+  btnRandom.disabled = true;
   const resp = await axios.get("https://randomuser.me/api/");
+  imgProfile.src = resp.data.message;
+
+  imgProfile.onload = () => {
+    btnRandom.innerText = "Generate more..";
+    btnRandom.disabled = false;
+  };
+
+  function selectgender() {
+    if (Gender === male) {
+      return "👨";
+    } else {
+      return "👩";
+    }
+  }
 }
+
+btnRandom.onclick = () => {
+  btnRandom.innerText = "Loading Profile..";
+  btnRandom.disabled = true;
+};
